da3f6630c3d47f9dcb18d10fb98afa967edb9d05
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-13 20:56:57 +0700

    Untuk menyimpan status UI dan repositoryMhs untuk mengelelola data mahasiswa

---
+++ app/src/main/java/com/example/meet_9/ui/viewModel/DetailMhsViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.meet_9.ui.viewModel
+
+import androidx.lifecycle.SavedStateHandle
+import com.example.meet_9.repository.RepositoryMhs
+
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+)
