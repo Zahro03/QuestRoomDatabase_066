b914bc3d831f575812575e604802ddacb14dc284
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-13 21:07:34 +0700

    DetailUiState adalah kelas data yang menyimpan status UI untuk detail mahasiswa

--- app/src/main/java/com/example/meet_9/ui/viewModel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/meet_9/ui/viewModel/DetailMhsViewModel.kt
@@ -1,9 +1,60 @@
 package com.example.meet_9.ui.viewModel
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.meet_9.repository.RepositoryMhs
+import com.example.meet_9.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
+) : ViewModel(){
+    private  val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMahasiswa(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+    fun deleteMhs(){
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let{
+            viewModelScope.launch {
+                repositoryMhs.deleteMahasiswa(it)
+            }
+        }
+    }
+}
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val erroeMessage: String =""
 )
