ddcf77318ee481983758b88094662f36b50481fc
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-13 19:04:24 +0700

    Digunakan agar saat ingin berpindah ke halaman home,cukup panggil DestinasiHome.route

--- app/src/main/java/com/example/meet_9/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/meet_9/ui/navigation/AlamatNavigasi.kt
@@ -3,4 +3,8 @@ package com.example.meet_9.ui.navigation
 
 interface  AlamatNavigasi{
         val  route: String
-    }
+}
+
+object DestinasiHome:AlamatNavigasi{
+    override val route = "home"
+}
