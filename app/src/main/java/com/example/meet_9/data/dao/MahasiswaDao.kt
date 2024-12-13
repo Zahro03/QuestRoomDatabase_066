bc0fe4d9193b2e88be05d015182a2eee0d171fa3
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-13 18:29:29 +0700

    Digunakan untuk mengambil data mahasiswa berdasarkan NIM yang diberikan

--- app/src/main/java/com/example/meet_9/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/meet_9/data/dao/MahasiswaDao.kt
@@ -1,13 +1,17 @@
 package com.example.meet_9.data.dao
 
 import androidx.room.Dao
-import androidx.room.Insert
+import androidx.room.Query
 import com.example.meet_9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface MahasiswaDao {
-    @Insert
-    suspend fun  insertMahasiswa(
-        mahasiswa: Mahasiswa
-    )
+interface  MahasiswaDao {
+    //getAllMahasiswa
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    //getMahasiswa
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String) : Flow<Mahasiswa>
 }
